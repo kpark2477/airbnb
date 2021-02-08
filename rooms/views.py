b6b0d9fc6eb5205565cdb13c880ec79ba92b119f
kyuwon park <kyuwonpark@kyuwonui-Macmini.local>
2021-02-09 01:36:48 +0900

    build views for rooms

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,31 @@
-from math import ceil
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
-from django.core.paginator import Paginator
 from . import models
 
 # Create your views here.
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms))
+class HomeView(ListView):
 
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = 'created'
+    context_object_name = "rooms"
+
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
+
+def search(request):
+    print(request)
+    return render(request, "rooms/search.html")
+
+
+
+
